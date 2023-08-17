ecb1e51b0cec27485d9ddf1aa3c10daf38ba1329
gajgeospatial <glen.johnson@gaj-geospatial.com>
2023-08-17 06:52:48 -0500

    Initial Check In

---
+++ msvc/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: N:/Development/Dev_Base/proj-9.2.0/msvc/googletest-src
+# Build directory: N:/Development/Dev_Base/proj-9.2.0/msvc/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ msvc/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: N:/Development/Dev_Base/proj-9.2.0/msvc/googletest-src/googletest
+# Build directory: N:/Development/Dev_Base/proj-9.2.0/msvc/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
